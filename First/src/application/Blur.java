bccc37be259b78311536a16f3d49bd737c92c923
Javier Fernández <Javier Fernndez @10.34.84.11>
2019-10-16 19:35:46 +0200

    JAVAFX

---
+++ First/src/application/Blur.java
@@ -0,0 +1,25 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.layout.Pane;
+
+public class Blur {
+	@FXML
+	private Pane pane;
+
+	@FXML
+	public void setfocus() {
+		pane.setEffect(new GaussianBlur(0));
+	}
+
+	@FXML
+	private void handleClose() {
+		System.exit(0);
+	}
+
+	@FXML
+	public void setblur() {
+		pane.setEffect(new GaussianBlur(5));
+	}
+}
